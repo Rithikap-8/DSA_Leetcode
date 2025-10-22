d7207c5772fa3a8b903c42a2dfa1e9ac663e3ddd
RITHIKA P <140421900+Rithikap-8@users.noreply.github.com>
2025-10-22 08:00:36 +0530

    Time: 3 ms (17.72%) | Memory: 43.3 MB (86.31%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+
+  Arrays.sort(nums);
+        List<Integer> list  = new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++)
+        {
+           list.add(nums[i]);
+        }
+        
+        int i=1;
+        while(list.contains(k*i))
+        {
+            i++;
+        }
+        return k*i;
+
+    }
+}
